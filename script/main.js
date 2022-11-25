7f6c3ab15338dd405506e4d64d32a6846d29fdfc
ryudg <ryudg95@gmail.com>
2022-11-25 16:08:08 +0900

    content
    + add content layout

--- script/main.js
+++ script/main.js
@@ -14,14 +14,19 @@ $(function () {
   );
 
   //이미지슬라이드
-  let img_num = 0;
+  let img_num = 0; // 이미지 번호
+  let img_height = 300;
+  let IntervalTime = 3000;
+
   setInterval(function () {
-    img_num++;
+    img_num++; //3000ms 마다 1씩 증가
+    // 마지막이면 처음으로
     if (img_num > 2) {
       img_num = 0;
     }
+    // 이동값 = 이미지번호 * 이미지 높이값
     $(".slider > .sliders").css({
-      transform: `translateY(${-img_num * 300}px)`,
+      transform: `translateY(${-img_num * img_height}px)`,
     });
-  }, 3000);
+  }, IntervalTime);
 });
