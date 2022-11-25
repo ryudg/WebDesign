41d62a9700059a93f34a2607ae1d7343a3e95112
ryudg <ryudg95@gmail.com>
2022-11-25 14:31:18 +0900

    add img
    add img

--- script/main.js
+++ script/main.js
@@ -2,11 +2,13 @@ $(function () {
   $("nav .depth1 > li").hover(
     function () {
       // mouseover
-      $(this).children().addClass("show");
+      // $(this).children().addClass("show");
+      $("nav .depth2").addClass("show");
     },
     function () {
       //mouseout
-      $(this).children().removeClass("show");
+      // $(this).children().removeClass("show");
+      $("nav .depth2").removeClass("show");
     }
   );
 });
