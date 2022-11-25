217556ec4a29660f1ec3496141fbdc80ea813b24
ryudg <ryudg95@gmail.com>
2022-11-25 15:10:10 +0900

    img slide
    add img slide

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 $(function () {
+  // 드롭다운 메뉴
   $("nav .depth1 > li").hover(
     function () {
       // mouseover
@@ -11,4 +12,16 @@ $(function () {
       $("nav .depth2").removeClass("show");
     }
   );
+
+  //이미지슬라이드
+  let img_num = 0;
+  setInterval(function () {
+    img_num++;
+    if (img_num > 2) {
+      img_num = 0;
+    }
+    $(".slider > .sliders").css({
+      transform: `translateY(${-img_num * 300}px)`,
+    });
+  }, 3000);
 });
